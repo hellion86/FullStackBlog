e174a6cef2dc7b83deb3361f888b6081e661f683
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2023-05-07 09:22:14 +0700

    change readme, add makefile

--- src/index.js
+++ src/index.js
@@ -26,45 +26,45 @@ import { validationResult } from 'express-validator';
 import UserModel from '../models/User.js';
 
 mongoose
-  .connect(
-    'mongodb+srv://vmezhurevsky:7NqUCHEhC2W58SfH@cluster0.uyeddc1.mongodb.net/blog'
-  )
-  .then(() => {
-    console.log('DB connect ok');
-  })
-  .catch((err) => {
-    console.log('DB error', err);
-  });
+    .connect(
+        'mongodb+srv://vmezhurevsky:7NqUCHEhC2W58SfH@cluster0.uyeddc1.mongodb.net/blog'
+    )
+    .then(() => {
+        console.log('DB connect ok');
+    })
+    .catch((err) => {
+        console.log('DB error', err);
+    });
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
 
-  const { password, email, fullName, avatarUrl } = req.body;
-  const passwordSalt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, passwordSalt);
+    const { password, email, fullName, avatarUrl } = req.body;
+    const passwordSalt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, passwordSalt);
 
-  const doc = new UserModel({
-    email,
-    fullName,
-    passwordHash,
-    avatarUrl,
-  });
+    const doc = new UserModel({
+        email,
+        fullName,
+        passwordHash,
+        avatarUrl,
+    });
 
-  const user = await doc.save();
+    const user = await doc.save();
 
-  res.json(user);
+    res.json(user);
 });
 
 app.listen(4444, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('OK');
+    if (err) {
+        return console.log(err);
+    }
+    console.log('OK');
 });
